1cc323fa27047af3d77865eede75ae2a7db824df
ABDUL AZIZ <107160974+dulaziz15@users.noreply.github.com>
2024-09-20 10:06:05 +0700

    struktur control

---
+++ Pertemuan4/strukturKontrol.php
@@ -0,0 +1,46 @@
+<?php
+    $nilaiNumerik = 92;
+
+    if($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
+        echo "Nilai huruf: A";
+    } elseif ($nilaiNumerik >= 80 && $nilaiNumerik < 90) {
+        echo "Nilai huruf: B";
+    } elseif ($nilaiNumerik >= 70 && $nilaiNumerik < 80) {
+        echo "Nilai huruf: C";
+    } elseif ($nilaiNumerik < 70) {
+        echo "Nilai huruf: D";
+    }
+
+    $jarakSaatIni = 0;
+    $jarakTarget = 500;
+    $peningkatanHarian = 30;
+    $hari = 0;
+
+    while ($jarakSaatIni < $jarakTarget) {
+        $jarakSaatIni += $peningkatanHarian;
+        $hari++;
+    }
+    echo "<br>";
+    echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+
+    $jumlahLahan = 10;
+    $tanamanPerlahan = 5;
+    $buahPerTamanan = 10;
+    $jumlahBuah = 0;
+
+    for ($i = 0; $i < $jumlahLahan; $i++) {
+        $jumlahBuah += ($tanamanPerlahan * $buahPerTamanan);
+    }
+    echo "<br>";
+    echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
+    $skorUjian = [85, 92, 78, 96, 88];
+    $totalSkor = 0;
+
+    foreach ($skorUjian as $skor) {
+        $totalSkor += $skor;
+    }
+
+    echo "<br>";
+    echo "Total skor ujian adalah: $totalSkor";
+?>
