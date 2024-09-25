48a56244b419008ae4d69d333fa2ac5fba0cc7b8
ABDUL AZIZ <107160974+dulaziz15@users.noreply.github.com>
2024-09-25 22:43:11 +0700

    soal praktikum 4.8

---
+++ Pertemuan4/soalPraktikum4.8.php
@@ -0,0 +1,7 @@
+<?php
+    $score = 600;
+    $hadiah = ($score > 500) ? 'IYA' : 'TIDAK';
+
+    echo "Total score pemain adalah : $score <br>";
+    echo "Apakah pemain mendapatkan hadiah tambahan ? $hadiah";
+?>
