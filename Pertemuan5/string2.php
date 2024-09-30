3a740009da0ad666b7faf18b19e8fefec449a744
ABDUL AZIZ <107160974+dulaziz15@users.noreply.github.com>
2024-09-30 22:42:27 +0700

    string 2

---
+++ Pertemuan5/string2.php
@@ -0,0 +1,12 @@
+<?php
+    echo "Barus\nbaru <br>";
+    echo 'Barus\nbaru <br>';
+    echo "Barus\rbaru <br>";
+    echo 'Barus\rbaru <br>';
+
+    echo "<pre>Helo\tDunia!</pre>";
+    echo '<pre>Helo\tDunia!</pre>';
+
+    echo "Katakanlah \"Tidak pada narkoba!\" <br>";
+    echo 'Katakanlah \'Tidak pada narkoba!\' <br>';
+?>
